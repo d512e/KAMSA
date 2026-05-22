43a352590fcd0fdd6479055ed713c8a052f8b10f
Sidy TRAORE <siditraore@MacBook-Pro-de-Sidi.local>
2026-05-22 06:17:36 +0000

    Second commit

--- Kamsa/settings.py
+++ Kamsa/settings.py
@@ -123,3 +123,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.100.87']
+

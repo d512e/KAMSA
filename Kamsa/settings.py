d242aa917e4034e8088f748eaaeff0e3ffa065d4
Bint <dside0205@gmail.com>
2026-07-08 00:52:08 +0000

    settings-correct

--- Kamsa/settings.py
+++ Kamsa/settings.py
@@ -116,8 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'Kamsa_auto/static']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'Kamsa_auto' / 'static',]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
